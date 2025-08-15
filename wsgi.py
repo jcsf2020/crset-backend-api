eb0b204068f643889c9ac11461c9e91a09d532ec
CRSET Solutions <crsetsolutions@gmail.com>
2025-08-15 11:12:41 -0400

    fallback: use waitress instead of gunicorn

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,9 @@
-# wsgi.py
+# wsgi.py - Fallback Waitress
 import os
 from src.main import app  # garante import explícito do objeto Flask
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.getenv("PORT", "8000")))
+    from waitress import serve
+    port = int(os.getenv("PORT", "8000"))
+    serve(app, host="0.0.0.0", port=port, threads=16)
 
